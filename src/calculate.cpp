4a9b2e5703c9ad350dfa6da6326b3766a779cd0a
Meryl Porsha <merylpor@un-k5.kzn.21-school.ru>
2023-04-24 00:02:38 +0300

    working on check class

--- src/calculate.cpp
+++ src/calculate.cpp
@@ -7,16 +7,34 @@ int Checks::check_operator(char value) {
   return operator_symbols.find(value) != string::npos;
 }
 int Checks::check_funcs(char value) {
-  return str_symbols.find(value) != string::npos;
+  // {"COS", 11},  {"SIN", 12},  {"TAN", 13}, {"ACOS", 14}, {"ASIN", 15},
+  //     {"ATAN", 16}, {"SQRT", 17}, {"LN", 18},  {"LOG", 19},  {"MOD", 2}};
+
+  return math_sympols.find(value) != string::npos;
 }
 int Checks::check_input(string vlaue) {
   int valid_operator = 0;
+  int scopes = 0;
+  unsigned int math_symb = 0;
   for (auto &i : vlaue) {
-    if (!check_number(i) && !check_operator(i)) return 0;
+    if (!check_number(i) && !check_operator(i) && !check_funcs(i)) return 0;
     if (check_number(i)) valid_operator = 0;
+    if (check_funcs(i)) {
+      ++math_symb;
+      --valid_operator;
+    };
     if (check_operator(i) && valid_operator) return 0;
-    if (check_operator(i)) ++valid_operator;
+    if (check_operator(i)) {
+      if (i == '(') {
+        if (math_symb) --math_symb;
+        ++scopes;
+      } else if (i == ')') {
+        --scopes;
+      } else {
+        ++valid_operator;
+      }
+    }
   }
-  if (valid_operator) return 0;
+  if (valid_operator || scopes || math_symb) return 0;
   return 1;
 }
--- src/test_calculate.cc
+++ src/test_calculate.cc
@@ -10,10 +10,13 @@ class TestCalculate : public ::testing::Test {
 };
 
 TEST_F(TestCalculate, inputValidationTest) {
-  int r;
+  // {"COS", !},  {"SIN", @},  {"TAN", #}, {"ACOS", $}, {"ASIN", _},
+  //     {"ATAN", v}, {"SQRT", b}, {"LN", u},  {"LOG", p},  {"MOD", %}};
   EXPECT_EQ(check.check_input("12543"), 1);
   EXPECT_EQ(check.check_input("125^+43"), 0);
   EXPECT_EQ(check.check_input("12..5^43"), 0);
   EXPECT_EQ(check.check_input("12.543"), 1);
   EXPECT_EQ(check.check_input("12.543^"), 0);
+  EXPECT_EQ(check.check_input("((2-1))"), 1);
+  // EXPECT_EQ(check.check_input("12.543((2-1))+1"), 1);
 }
