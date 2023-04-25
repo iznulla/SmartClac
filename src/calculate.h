cdf0e79d0e0a284550206fb498256a1b1a7c1d94
Meryl Porsha <merylpor@un-k5.kzn.21-school.ru>
2023-04-26 01:12:10 +0300

    calcing

--- src/calculate.h
+++ src/calculate.h
@@ -1,12 +1,15 @@
+#include <cstdlib>
 #include <iostream>
 #include <list>
 #include <map>
+#include <stack>
 #include <string>
 
 using namespace std;
 
-struct Node_data {
-  double value;
+struct Node_operator {
+  char value;
+  int priority = 0;
 };
 
 class Checks {
@@ -24,10 +27,112 @@ class Checks {
   string digit_symbols = "0123456789";
   string operator_symbols = "+-*/()^.%";
   // string str_symbols = "sincoatqrlgmd";
-  map<string, int> math_preorites{
-      {"cos", 11},  {"sin", 12},  {"tan", 13}, {"acos", 14}, {"asin", 15},
-      {"atan", 16}, {"sqrt", 17}, {"ln", 18},  {"log", 19},  {"mod", 2}};
+  map<string, int> math_preorites{{"!", 11}, {"@", 12}, {"#", 13}, {"$", 14},
+                                  {"_", 15}, {"v", 16}, {"b", 17}, {"u", 18},
+                                  {"p", 19}, {"%", 2}};
   // {"COS", !},  {"SIN", @},  {"TAN", #}, {"ACOS", $}, {"ASIN", _},
   //     {"ATAN", v}, {"SQRT", b}, {"LN", u},  {"LOG", p},  {"MOD", %}};
   string math_sympols = "!@#$_vbup";
+};
+
+class Calculate {
+ public:
+  Calculate() = default;
+  ~Calculate() = default;
+
+  void clear() {
+    this->node_.clear();
+    while (!opr_.empty()) {
+      this->opr_.pop();
+    }
+  }
+  double plus(double x, double y) { return x + y; }
+  double sub(double x, double y) { return x - y; }
+  double mul(double x, double y) { return x * y; }
+  double div(double x, double y) { return x / y; }
+
+  double calc(double x, double y, char op) {
+    if (op == '+')
+      return plus(x, y);
+    else if (op == '-')
+      return sub(x, y);
+    else if (op == '*')
+      return mul(x, y);
+    else if (op == '/')
+      return div(x, y);
+    else
+      return 0;
+  }
+  double pop_item() {
+    double x = node_.back();
+    node_.pop_back();
+    return x;
+  }
+
+  int priority(char op) {
+    int result = 10;
+    if (op == '+' || op == '-')
+      result = 1;
+    else if (op == '/' || op == '*')
+      result = 2;
+    else if (op == '(')
+      result = 0;
+    return result;
+  }
+
+  void calculate(char op) {
+    double y = pop_item();
+    double x = pop_item();
+    node_.push_back(calc(x, y, opr_.top()));
+    opr_.pop();
+  }
+
+  void add_item(char op) {
+    if (!opr_.empty()) {
+      if (priority(op) <= priority(opr_.top())) {
+        calculate(opr_.top());
+        if (op != 0) opr_.push(op);
+      } else {
+        opr_.push(op);
+      }
+    } else {
+      opr_.push(op);
+    }
+  }
+
+  void pars_and_calc(string value) {
+    if (check.input_check(value)) {
+      double d;
+      for (size_t i = 0; i != value.size();) {
+        if (check.number_check(value[i])) {
+          std::size_t t = i;
+          d = stod(value.substr(t), &t);
+          i += t;
+          this->node_.push_back(d);
+        } else if (value[i] == '(' || value[i] == ')') {
+          ++i;
+          continue;
+        } else {
+          add_item(value[i]);
+          i++;
+        }
+      }
+      calculate(opr_.top());
+    }
+  }
+  void print() {
+    for (auto &i : node_) cout << i << endl;
+  }
+
+  double calcAll(string value) {
+    pars_and_calc(value);
+    if (node_.size() > 1) calculate(opr_.top());
+    double res = node_.back();
+    return res;
+  }
+
+ private:
+  Checks check{};
+  list<double> node_{};
+  stack<char> opr_{};
 };
--- src/test_calculate.cc
+++ src/test_calculate.cc
@@ -7,6 +7,7 @@
 class TestCalculate : public ::testing::Test {
  public:
   Checks check;
+  Calculate calc;
 };
 
 TEST_F(TestCalculate, inputValidationTest) {
@@ -41,4 +42,56 @@ TEST_F(TestCalculate, inputValidationTest) {
   EXPECT_EQ(check.input_check("+2*5-2"), 1);
   EXPECT_EQ(check.input_check(".2*5-2"), 0);
   EXPECT_EQ(check.input_check("@(2*5-2)"), 1);
+}
+
+TEST_F(TestCalculate, ParsingTest) {
+  // {"COS", !},  {"SIN", @},  {"TAN", #}, {"ACOS", $}, {"ASIN", _},
+  //     {"ATAN", v}, {"SQRT", b}, {"LN", u},  {"LOG", p},  {"MOD", %}};
+  // double result = calc.calcAll("2.11+1-3");
+  EXPECT_DOUBLE_EQ(calc.calcAll("2+1-3+9"), 9);
+  calc.clear();
+  EXPECT_DOUBLE_EQ(calc.calcAll("2*1+3"), 5);
+  calc.clear();
+  EXPECT_DOUBLE_EQ(calc.calcAll("2*1+3/1"), 5);
+  calc.clear();
+  EXPECT_DOUBLE_EQ(calc.calcAll("2+1+3/2"), 4.5);
+  calc.clear();
+  EXPECT_DOUBLE_EQ(calc.calcAll("2+1-3+9*12/3+31*241"), 7507);
+  calc.clear();
+  EXPECT_DOUBLE_EQ(calc.calcAll("2+1-3+2*2*(3+1)*2"), 32);
+  calc.clear();
+  EXPECT_DOUBLE_EQ(calc.calcAll("2+1-3+9*12"), 108);
+  // calc.print();
+  // EXPECT_EQ(calc.init_stack("1+2"), 3);
+  // EXPECT_EQ(check.input_check("125^+43"), 0);
+  // EXPECT_EQ(check.input_check("12..5^43"), 0);
+  // EXPECT_EQ(check.input_check("12.543"), 1);
+  // EXPECT_EQ(check.input_check("12.543^"), 0);
+  // EXPECT_EQ(check.input_check("((2-1))"), 1);
+  // EXPECT_EQ(check.input_check("((2-1))1"), 0);
+  // EXPECT_EQ(check.input_check("((2-1)-)"), 0);
+  // EXPECT_EQ(check.input_check("((2-1))+1"), 1);
+  // EXPECT_EQ(check.input_check("12.543+((2-1))"), 1);
+  // EXPECT_EQ(check.input_check("12.543((2-1))"), 0);
+  // EXPECT_EQ(check.input_check("12.543+!(@(2-1)+12)"), 1);
+  // EXPECT_EQ(check.input_check("12.543+!(@(2-1)+12-)"), 0);
+  // EXPECT_EQ(check.input_check("12.543+!(2+@(2-1)+12)543+!(2+@(2-1))"), 0);
+  // EXPECT_EQ(check.input_check("12.543+!(2+@(2-1)+12)+543+!(2+@(2-1))"), 1);
+  // EXPECT_EQ(
+  //     check.input_check("@(4)+9.235+2^3^!(#(10))-564-@(25)+28.6*35%99^2^3"),
+  //     1);
+  // EXPECT_EQ(
+  //     check.input_check("5+(1+2)*4*(1)*@(2*7.5-2)+!(@(2*5))-#(2^_(5-1))+$(55)"),
+  //     1);
+  // EXPECT_EQ(
+  //     check.input_check("1+2+3+(3+5)*2+3/(1-4)^3*(3^((2+1)+1))/(((1)))+2"),
+  //     1);
+  // // EXPECT_EQ(
+  // //
+  // check.input_check("5+(1+2)*4*(1)*@(.2*5-2)+!(@(2*5))-#(2^_(5-1))+$(*55)"),
+  // //     0);
+  // EXPECT_EQ(check.input_check("(-2*5-2)"), 1);
+  // EXPECT_EQ(check.input_check("+2*5-2"), 1);
+  // EXPECT_EQ(check.input_check(".2*5-2"), 0);
+  // EXPECT_EQ(check.input_check("@(2*5-2)"), 1);
 }
