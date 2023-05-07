90bdda9d39e39ba253438f739d4c44d4e14e03ab
Meryl Porsha <merylpor@un-k5.kzn.21-school.ru>
2023-05-07 20:45:15 +0300

    credit calcing ok

--- src/credit.cpp
+++ src/credit.cpp
@@ -1,71 +1,52 @@
-#include <math.h>
+#include "credit.h"
 
-#include <iostream>
-#include <vector>
+double Credit::termAndRate(double rate_, double term_) {
+  double result;
+  result = pow((1.0 + rate(rate_)), term_) - 1;
+  return result;
+}
+double Credit::oneMonthAnnu(double ammo_, double rate_, double term_) {
+  double result;
+  result = ammo_ * (rate(rate_) + (rate(rate_) / termAndRate(rate_, term_)));
+  return result;
+}
 
-class Credit {
- public:
-  Credit() = default;
-  ~Credit() = default;
-
- public:
-  double rate(double value_) { return value_ / 100 / 12; }
-  double termAndRate(double rate_, double term_) {
-    double result;
-    result = pow((1.0 + rate(rate_)), term_) - 1;
-    return result;
+std::pair<std::vector<double>, std::vector<double>> Credit::annuCalc(
+    double ammo_, double rate_, double term_) {
+  std::vector<double> debt_p{}, balance_o{};
+  double amm = oneMonthAnnu(ammo_, rate_, term_);
+  double balance_owed = ammo_;
+  double debt_part;
+  for (auto i = 0.0; i < term_; ++i) {
+    debt_part = amm - percentage(balance_owed, rate_);
+    balance_owed -= debt_part;
+    debt_p.push_back(debt_part);
+    balance_o.push_back(balance_owed);
   }
-  double oneMonthAnnu(double ammo_, double rate_, double term_) {
-    double result;
-    result = ammo_ * (rate(rate_) + (rate(rate_) / termAndRate(rate_, term_)));
-    return result;
+  return std::pair(debt_p, balance_o);
+}
+
+std::pair<std::vector<double>, std::vector<double>> Credit::difCalc(
+    double ammo_, double rate_, double term_) {
+  std::vector<double> debt_p{}, balance_o{};
+  double amm = oneMonthDif(ammo_, term_);
+  double balance_owed = ammo_;
+  double debt_part;
+  for (auto i = 0.0; i < term_; ++i) {
+    debt_part = amm + percentage(balance_owed, rate_);
+    balance_owed -= amm;
+    debt_p.push_back(debt_part);
+    balance_o.push_back(balance_owed);
   }
-  double percentage(double ammo, double rate_) { return ammo * rate(rate_); }
-
-  std::pair<std::vector<double>, std::vector<double>> annuCalc(double ammo_,
-                                                               double rate_,
-                                                               double term_) {
-    std::vector<double> debt_p{}, balance_o{};
-    double amm = oneMonthAnnu(ammo_, rate_, term_);
-    double balance_owed = ammo_;
-    double debt_part;
-    for (auto i = 0.0; i < term_; ++i) {
-      debt_part = amm - percentage(balance_owed, rate_);
-      balance_owed -= debt_part;
-      debt_p.push_back(debt_part);
-      balance_o.push_back(balance_owed);
-    }
-    return std::pair(debt_p, balance_o);
-  }
-
-  double oneMonthDif(double ammo_, double term_) { return ammo_ / term_; }
-
-  void difCalc(double ammo_, double rate_, double term_) {
-    std::vector<double> debt_p{}, balance_o{};
-    double amm = oneMonthDif(ammo_, term_);
-    double balance_owed = ammo_;
-    double debt_part;
-    for (auto i = 0.0; i < term_; ++i) {
-      debt_part = amm + percentage(balance_owed, rate_);
-      balance_owed -= amm;
-      debt_p.push_back(debt_part);
-      balance_o.push_back(balance_owed);
-    }
-  }
-
- private:
-  double loan_ammount;
-  double interest_rate;
-
-  int term;
-};
+  return std::pair(debt_p, balance_o);
+}
 
 int main() {
   Credit cr;
   std::cout << "Платеж Долговая часть остаток" << std::endl;
-  auto ann = cr.annuCalc(200000, 12, 24);
-  for (auto &i : &j : ann.first, ann.second) {
-    std::cout << i << std::endl;
+  auto ann = cr.difCalc(200000, 12, 24);
+  for (auto i = 0; i < ann.first.size(); ++i) {
+    std::cout << ann.first[i] << " " << ann.second[i] << std::endl;
   }
   return 0;
 }
---
+++ src/credit.h
@@ -0,0 +1,26 @@
+#include <math.h>
+
+#include <iostream>
+#include <vector>
+
+class Credit {
+ public:
+  Credit() = default;
+  ~Credit() = default;
+
+ public:
+  double rate(double value_) { return value_ / 100 / 12; }
+  double termAndRate(double rate_, double term_);
+  double oneMonthAnnu(double ammo_, double rate_, double term_);
+  double percentage(double ammo, double rate_) { return ammo * rate(rate_); }
+
+  std::pair<std::vector<double>, std::vector<double>> annuCalc(double ammo_,
+                                                               double rate_,
+                                                               double term_);
+
+  double oneMonthDif(double ammo_, double term_) { return ammo_ / term_; }
+
+  std::pair<std::vector<double>, std::vector<double>> difCalc(double ammo_,
+                                                              double rate_,
+                                                              double term_);
+};
